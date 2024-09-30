aeaeadeb8b65d65ffad34148745acdca17efcd88
ricar <ricar@Ricardo>
2024-09-30 15:18:05 +0200

    Adding Greeging persistence

--- reparto/src/main/java/es/uclm/reparto/business/controller/GreetingController.java
+++ reparto/src/main/java/es/uclm/reparto/business/controller/GreetingController.java
@@ -1,5 +1,8 @@
 package es.uclm.reparto.business.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,19 +10,30 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import es.uclm.reparto.business.entity.Greeting;
+import es.uclm.reparto.persistency.GreetingDAO;
 
 @Controller
 public class GreetingController {
 
+	private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
+	@Autowired
+	private GreetingDAO greetingDAO;
+
+	
+	
 	@GetMapping("/greeting")
 	public String greetingForm(Model model) {
 		model.addAttribute("greeting", new Greeting());
+		log.info(greetingDAO.findAll().toString());
 		return "greeting";
 	}
 
 	@PostMapping("/greeting")
 	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
 		model.addAttribute("greeting", greeting);
+		Greeting savedGreeting = greetingDAO.save(greeting);
+		log.info("Saved greeting: " + savedGreeting);
 		return "result";
 	}
 }
