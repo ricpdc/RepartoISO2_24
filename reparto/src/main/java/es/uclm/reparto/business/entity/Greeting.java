fa0913f05eb0d6b43203e7dc2fc53e7cf9c40dc6
ricar <ricar@Ricardo.uclm.es>
2024-10-03 11:45:54 +0200

    Adding Greeting persistency

--- reparto/src/main/java/es/uclm/reparto/business/entity/Greeting.java
+++ reparto/src/main/java/es/uclm/reparto/business/entity/Greeting.java
@@ -1,8 +1,32 @@
 package es.uclm.reparto.business.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Greeting {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	@Column
 	private String person;
+
+	@Column
 	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String person, String content) {
+		super();
+		this.person = person;
+		this.content = content;
+	}
 
 	public String getPerson() {
 		return person;
@@ -20,4 +44,11 @@ public class Greeting {
 		this.content = content;
 	}
 
+	@Override
+	public String toString() {
+		return "Greeting [id=" + id + ", person=" + person + ", content=" + content + "]";
+	}
+
+	
+	
 }
